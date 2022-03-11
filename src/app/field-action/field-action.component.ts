e30e189c8adcfda772fef67c7bbc85c33881f2b5
Felipe Luiz <lipao.gamer@gmail.com>
2022-03-11 16:57:29 -0300

    adicionado conteúdo, estilização e responsividade da página field-action, retificado responsividade do logo, alterado o tamanho geral do h1 de cada página

--- src/app/field-action/field-action.component.ts
+++ src/app/field-action/field-action.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-field-action',
   templateUrl: './field-action.component.html',
-  styleUrls: ['./field-action.component.scss']
+  styleUrls: ['./field-action.component.scss'],
 })
 export class FieldActionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  civel = ['algo1', 'algo2', 'algo3', 'algo4'];
+  familia = ['algo1', 'algo2', 'algo3', 'algo4'];
+  sucessoes = ['algo1', 'algo2', 'algo3', 'algo4'];
+  digital = ['algo1', 'algo2', 'algo3', 'algo4'];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
