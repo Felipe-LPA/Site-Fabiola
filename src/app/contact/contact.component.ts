954800fc672f5416b45bf2001f52a41ed4a65a3e
Felipe Luiz <lipao.gamer@gmail.com>
2022-03-25 18:27:45 -0300

    adicionado reactive Form, alterado alguns elementos e estilizações

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  optEmailPhone = "optEmailPhone"
+  constructor(private formBuilder: FormBuilder) { }
+  contactForm = this.formBuilder.group({
+    name: ["",[Validators.required]],
+    subject: [""],
+    optEmailPhone: ["optEmailPhone"],
+    email: ["",[]],
+    phone: ["",[]],
+    description: ['']
+  })
+  // phoneConditionallyRequiredValidator(formControl: AbstractControl){
+  //   if(this.optEmailPhone == "optEmailPhone") return Validators.required(formControl)
+  //   return null
+  // }
 
+  // emailConditionallyRequiredValidator(formControl: AbstractControl) {
+  //   if (!formControl.parent || !formControl || formControl.parent.get('optEmailPhone') == null) {
+  //     return null;
+  //   }
+    
+  //   if (formControl.parent.get('optEmailPhone').value == "optEmailPhone") {
+  //     return Validators.required(formControl); 
+  //   }
+  //   return null;
+  // }
+
+  // phoneConditionallyRequiredValidator() {
+  //   if(this.optEmailPhone == 'optEmailPhone' || this.optEmailPhone == 'optPhone'){
+  //     Validators.
+  //   }
+  //   return null
+  // }
+  
+  onSubmit(){
+    console.log(this.contactForm.value)
+  }
   ngOnInit(): void {
+    
   }
 
 }
